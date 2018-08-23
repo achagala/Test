63095af970da8629bece7a7f106a2147b628399a
Alumno <Alumno@ws204-10>
2018-08-22 18:19:59 -0700

    Agregar nuevo atributo

--- src/Paq1/Operaciones.java
+++ src/Paq1/Operaciones.java
@@ -1,7 +1,15 @@
 package Paq1;
 
 public enum Operaciones {
-	SUMA, RESTA, MULTIPLICACION, DIVISION;
+	SUMA("+"), RESTA("-"), MULTIPLICACION("*"), DIVISION("/");
 	
+	private String valor;
 	
+	Operaciones(String valor){
+		this.valor=valor;
+	}
+	
+	public String getValor(){
+		return valor;
+	}
 }
--- src/Paq1/Test.java
+++ src/Paq1/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(Operaciones.MULTIPLICACION);
+		System.out.println(Operaciones.SUMA.getValor());
+		System.out.println(Operaciones.SUMA.name());
 	}
 
 }
